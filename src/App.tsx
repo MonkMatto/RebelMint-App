f1a149fbbfba76aa82f9d8c660becd3326d6226a
tophercully <89924670+tophercully@users.noreply.github.com>
2024-06-10 23:28:47 +0200

    fix type errors, build

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { useEffect, useState } from 'react'
 import RebelMint from './RebelMint/src/RebelMint'
-import { useLocation, useSearchParams } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 
 function App() {
-    const location = useLocation()
     const [searchParams, setSearchParams] = useSearchParams()
     const contractAddress = searchParams.get('contract')
-    const [inputAddress, setInputAddress] = useState()
-    useEffect(()=> {
-
-    }, [inputAddress])
+    const [inputAddress, setInputAddress] = useState<string>()
+    useEffect(() => {}, [inputAddress])
     //0xfbE3687896B583E9E9727a58BD96207f35fD015c
     if (contractAddress) {
         return (
@@ -21,17 +18,24 @@ function App() {
         return (
             <>
                 <h1>Please enter a Contract Address</h1>
-                <p>/?contract=0xfbE3687896B583E9E9727a58BD96207f35fD015c</p>
-                <form onSubmit={(e)=> {
-                  e.preventDefault()
-                  setSearchParams({contract: inputAddress})
-                  // setSearchParams({e.target.value})
-                }}>
-
-                <input className='bg-black' onChange={(e)=>{setInputAddress(e.target.value)}}></input>
-                <button type='submit'></button>
+                <p>/?contract=0x62d8cf316aD924D887533e67BC7067De3cE124eB</p>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        setSearchParams(
+                            inputAddress ? { contract: inputAddress } : ''
+                        )
+                        // setSearchParams({e.target.value})
+                    }}
+                >
+                    <input
+                        className="bg-black"
+                        onChange={(e) => {
+                            setInputAddress(e.target.value)
+                        }}
+                    ></input>
+                    <button type="submit"></button>
                 </form>
-
             </>
         )
     }
